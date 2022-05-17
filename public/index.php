a6a6d5936d9ec942e86826e92d019a4d3cd1e53d
Nate Bate <LetsInnovate.io@gmail.com>
2022-05-17 12:13:13 -0400

    Basic Functionality Working

--- public/index.php
+++ public/index.php
@@ -3,6 +3,15 @@ define('SRC_PATH',dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEP
 define('VENDOR_PATH', __DIR__ . '/../vendor');
 
 require_once VENDOR_PATH.'/autoload.php';
+
+// Load dotenv?
+if (class_exists('Dotenv\Dotenv') && file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+} else {
+    exit("Either the .env file does not exist, or the Dotenv class file does not exist.  Exiting.");
+}
+
 require SRC_PATH . "SmsDemo.php";
 
 SmsDemo::sendText();
--- src/SmsDemo.php
+++ src/SmsDemo.php
@@ -1,11 +1,38 @@
 <?php
 
 
+use Twilio\Rest\Client;
+
 class SmsDemo {
 
     public static function sendText()
     {
-        echo "got here";
+        $sid = getenv("TWILIO_ACCOUNT_SID");
+        $token = getenv('TWILIO_AUTH_TOKEN');
+
+        $twilio = new Client($sid, $token);
+
+        $demo_phone_number = getenv('TWILIO_DEMO_PHONE_NUMBER');
+        $message_service_id = getenv("TWILIO_MESSAGING_SERVICE_ID");
+        $twilio_number = getenv("TWILIO_PHONE_NUMBER");
+        $from_header = getenv("TWILIO_FROM_LABEL");
+
+        $message = $twilio->messages
+            ->create(
+                $demo_phone_number,
+                array(
+                    'from' => $twilio_number,
+                    "messagingServiceSid" => $message_service_id,
+                    "body" => "$from_header This is a demo text from Let's Innovate!"
+                )
+            );
+
+        print("Status Response from message sending: <br>");
+        print("<pre>");
+        print_r($message->status);
+        print("</pre>");
+
     }
+
 }
 
